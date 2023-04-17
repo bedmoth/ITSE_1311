70c307c73ec134e97d3db26a0730c099dfbafb01
dani bishop <danibishoptx@gmail.com>
2023-04-17 20:02:16 +0000

    Add C10 files

---
+++ music/scripts/script.js
@@ -0,0 +1,13 @@
+/*
+Student Name:Dani Bishop
+File Name: script.js
+Date:04/14/2023
+*/
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
